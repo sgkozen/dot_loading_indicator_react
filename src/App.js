2884fef5623cbd571322f2d48641c23804a86ea2
sgkozen <rikasean@gmail.com>
2025-07-25 13:26:31 +0900

    Changes dot loader box size

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import DotLoader from './DotLoader';
 
 function App() {
--- src/DotLoader.js
+++ src/DotLoader.js
@@ -2,18 +2,18 @@ import React from "react";
 import styles from "./DotLoader.module.css";
 
 const DotLoader = () => {
-  return (
-    
-      <div className={styles.dot_loader}>
-        <span className={styles.dot_loader_balls}></span>
-        <span className={styles.dot_loader_balls}></span>
-        <span className={styles.dot_loader_balls}></span>
-        <span className={styles.dot_loader_balls}></span>
-        <span className={styles.dot_loader_balls}></span>
-        <span className={styles.dot_loader_balls}></span>
-      </div>
-    
-  );
+	return (
+		<div className={styles.dot_loader_box}>
+			<div className={styles.dot_loader}>
+				<span className={styles.dot_loader_balls}></span>
+				<span className={styles.dot_loader_balls}></span>
+				<span className={styles.dot_loader_balls}></span>
+				<span className={styles.dot_loader_balls}></span>
+				<span className={styles.dot_loader_balls}></span>
+				<span className={styles.dot_loader_balls}></span>
+			</div>
+		</div>
+	);
 };
 
 export default DotLoader;
